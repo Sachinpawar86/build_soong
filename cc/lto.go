fb62a44ff531f9b1f4453cc555f1a7c1314c340b
root <root@localhost>
2025-11-16 08:56:50 +0000

    Revert "lto: enable O3 optimizations on explicit lto modules"
    This reverts commit f5562bbd721d84b76fd3492a311ce7a974e3c1f5.

--- cc/lto.go
+++ cc/lto.go
@@ -108,15 +108,11 @@ func (lto *lto) flags(ctx ModuleContext, flags Flags) Flags {
 	}
 	if lto.Properties.LtoEnabled {
 		ltoCFlags := []string{"-flto=thin", "-fsplit-lto-unit"}
-		var ltoCOnlyFlags []string
 		var ltoLdFlags []string
 
 		// Do not perform costly LTO optimizations for Eng builds.
 		if Bool(lto.Properties.Lto_O0) || ctx.Config().Eng() {
 			ltoLdFlags = append(ltoLdFlags, "-Wl,--lto-O0")
-		} else {
-			ltoLdFlags = append(ltoLdFlags,"-Wl,--lto-O3")
-			ltoCOnlyFlags = append(ltoCOnlyFlags, "-O3")
 		}
 
 		// Utilize unified LTO for greater optimization than ThinLTO with a
@@ -166,7 +162,6 @@ func (lto *lto) flags(ctx ModuleContext, flags Flags) Flags {
 		flags.Local.AsFlags = append(flags.Local.AsFlags, ltoCFlags...)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoCFlags...)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoLdFlags...)
-		flags.Local.CFlags = append(flags.Local.CFlags, ltoCOnlyFlags...)
 	}
 	return flags
 }
